52d2f286671495ecdc3bba7fa28eaa420647419d
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-08-04 15:54:39 -0300

    Commit 5

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,38 +1,63 @@
-import { ReactNode, useContext } from "react";
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { List } from "./List";
 import { Register } from "./Register";
 import { Breed } from "./Breed";
 import { UserContextContext } from "../context/userToken/index";
 
 export function RouterApp() {
-  const { dataUserContext, setDataUserContext } = useContext(UserContextContext);
+  const { dataUserContext } = useContext(UserContextContext);
+
+  const defaultProtectedRouteProps: Omit<ProtectedRouteProps, "outlet"> = {
+    isAuthenticated: dataUserContext.length !== 0,
+    authenticationPath: "/",
+  };
+
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Register />} />
-        <Route path="list" element={<List />} />
-        <Route path="list/:breed" element={<Breed />} />
+
+        {/* <Route path="list" element={<List />} /> */}
+        {/* <Route path="list/:breed" element={<Breed />} /> */}
+        <Route
+          path="list"
+          element={
+            <ProtectedRoute
+              {...defaultProtectedRouteProps}
+              outlet={<List />}
+            />
+          }
+        />
+        <Route
+          path="list/:breed"
+          element={
+            <ProtectedRoute
+              {...defaultProtectedRouteProps}
+              outlet={<Breed />}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
 }
 
-function PrivateOutlet() {
-  const auth = useAuth();
-  return auth ? <Outlet /> : <Navigate to="/" />;
-}
+export type ProtectedRouteProps = {
+  isAuthenticated: boolean;
+  authenticationPath: string;
+  outlet: JSX.Element;
+};
 
-interface PrivateRouteProps {
-  children: ReactNode;
-}
-
-function PrivateRoute({ children }: PrivateRouteProps) {
-  const auth = useAuth();
-  return auth ? children : <Navigate to="/login" />;
-}
-
-function useAuth() {
-  return true;
+export default function ProtectedRoute({
+  isAuthenticated,
+  authenticationPath,
+  outlet,
+}: ProtectedRouteProps) {
+  if (isAuthenticated) {
+    return outlet;
+  } else {
+    return <Navigate to={{ pathname: authenticationPath }} />;
+  }
 }
